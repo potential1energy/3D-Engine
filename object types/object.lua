462a5a9bcfc695196d8b915d06ad34dc307ed989
potential1energy <pikachu9playz@gmail.com>
2022-09-28 18:22:50 +1300

    Rearranged structure

--- object types/object.lua
+++ object types/object.lua
@@ -4,7 +4,7 @@ local object = class:derive('object')
 function object:new(name, verts, texture, pos, rot)
     local VertFormat = {{"VertexPosition", "float", 3}, {"VertexTexCoord", "float", 2}, {"VertexColor", "byte", 4}}
     self.verts = verts
-    self.texture = love.graphics.newImage(texture)
+    self.texture = texture
     self.mesh = love.graphics.newMesh(VertFormat, self.verts, "triangles")
     self.mesh:setTexture(self.texture)
     self.pos = pos or {0,0,0}
